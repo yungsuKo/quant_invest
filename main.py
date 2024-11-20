eaeacaa734954e996cfe93e788d31038cf01c093
yungsuKo <yungsu2391@gmail.com>
2024-11-21 00:49:17 +0900

    dd

--- main.py
+++ main.py
@@ -37,7 +37,24 @@ def get_cur_price(ticker):
 
 # print(ticker_list)
 
+### 보유한 코인량 조회
 def get_balance_coin(ticker):
     return upbit.get_balance(ticker)
 
-print(get_balance_coin(coin))
+### 평균 매수가 조회
+def get_buy_avg(ticker):
+    return upbit.get_avg_buy_price(ticker)
+
+### 주문 정보 조회
+def get_order_info(ticker):
+    try:
+        orders = upbit.get_order(ticker)
+        if "error" in orders[0]:
+            return None
+        return orders[-1]  # 마지막 주문건에 대한 정보 리턴
+    except Exception as e:
+        print(e)
+        return None
+
+# info는 원하는 symbol 을 입력하거나, 주문의 uuid 를 입력
+print(get_order_info(coin))
